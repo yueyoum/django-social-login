b637d2f569aaa725337947b0ac32e978325ecea3
yueyoum <yueyoum@gmail.com>
2013-04-05 01:40:33 +0800

    add version info, and change Admin

--- social_login/admin.py
+++ social_login/admin.py
@@ -4,12 +4,13 @@ from .app_settings import SOCIAL_LOGIN_ENABLE_ADMIN, SOCIAL_LOGIN_USER_INFO_MODE
 if SOCIAL_LOGIN_ENABLE_ADMIN:
     from django.contrib import admin
     from django.db.models import get_model
-    from .models import SocialUser
+    from .models import User, SocialUser
     
     info_model = get_model(*SOCIAL_LOGIN_USER_INFO_MODEL.split('.'))
-    class SocialUserAdmin(admin.ModelAdmin):
-        list_display = ('id', 'Username', 'site_uid', 'site_id', 'Avatar')
-        list_filter = ('site_id',)
+    class UserAdmin(admin.ModelAdmin):
+        list_display = ('id', 'Username', 'Avatar', 'is_social', 'is_active',
+                        'date_joined', 'SiteId')
+        list_filter = ('is_social',)
         
         def Username(self, obj):
             return info_model.objects.get(id=obj.id).username
@@ -18,5 +19,8 @@ if SOCIAL_LOGIN_ENABLE_ADMIN:
             return '<img src="%s" />' % info_model.objects.get(id=obj.id).avatar
         Avatar.allow_tags = True
         
+        def SiteId(self, obj):
+            return SocialUser.objects.get(id=obj.id).site_id
         
-    admin.site.register(SocialUser, SocialUserAdmin)
+        
+    admin.site.register(User, UserAdmin)
